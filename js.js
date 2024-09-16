9dfca1f4cbeaf60514852b5f11a9c6d5f4780f91
Kirill1810 <129986639+Kirill1810@users.noreply.github.com>
2024-09-16 18:05:26 +0300

    hgffg

---
+++ js.js
@@ -0,0 +1,56 @@
+
+// 2
+const mons = 30;
+console .log (mons*24)
+console .log (mons*24*60)
+
+
+// 3
+const heals = 100;
+const energy = 100;
+
+console .log ( heals -50 , energy - 50)
+
+
+// 4
+const cost = 1000;
+console .log ( cost - 10)
+
+// 5
+
+const numer = 59.255;
+
+console .log (Math.floor(numer))
+
+// 6
+
+
+
+const nume = 59.2;
+
+console .log (parseFloat(nume))
+
+
+
+// 7
+const ссс = 10;
+
+parseInt(ссс)
+
+console .log (parseInt(ссс))
+
+// 8
+
+const ссс2 = 16;
+
+console.log(Math.sqrt(ссс2))
+
+// 9
+
+const nu = 59.255;
+
+console .log (parseFloat(nu)  )
+console .log ( toString(nu) )
+
+
+
