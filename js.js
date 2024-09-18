6651e543169374c79eb66ac11486a27e88264a66
Kirill1810 <129986639+Kirill1810@users.noreply.github.com>
2024-09-18 17:06:20 +0300

    ggg

--- js.js
+++ js.js
@@ -1,56 +1,62 @@
+// 1
+
+let celsi = 25; 
+let farengeit; 
+
+farengeit = (celsi * 9/5) + 32;
+
+console.log( celsi);
+console.log( farengeit);
+
 
 // 2
 const mons = 30;
-console .log (mons*24)
-console .log (mons*24*60)
-
+console.log(mons * 24);
+console.log(mons * 24 * 60);
 
 // 3
 const heals = 100;
 const energy = 100;
 
-console .log ( heals -50 , energy - 50)
-
+console.log(heals - 50, energy - 50);
 
 // 4
 const cost = 1000;
-console .log ( cost - 10)
+console.log(cost - 10);
 
 // 5
 
 const numer = 59.255;
 
-console .log (Math.floor(numer))
+console.log(Math.floor(numer));
 
 // 6
 
-
-
 const nume = 59.2;
 
-console .log (parseFloat(nume))
-
-
+console.log(parseFloat(nume));
 
 // 7
-const ссс = 10;
+const num = 10.11;
 
-parseInt(ссс)
-
-console .log (parseInt(ссс))
+console.log(parseInt(num));
 
 // 8
 
-const ссс2 = 16;
+const sixteen = 16;
 
-console.log(Math.sqrt(ссс2))
+console.log(Math.sqrt(sixteen));
 
 // 9
 
-const nu = 59.255;
+let numStr = "42";
 
-console .log (parseFloat(nu)  )
-console .log ( toString(nu) )
+let duble;
 
+duble = parseInt(numStr);
 
+console.log(duble);
 
+let str = duble.toString();
+
+console.log(str);
